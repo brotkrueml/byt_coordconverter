36ece7feec02fdcc3aecd6ef0281872b84337c45
Chris Müller <chm@gmx.li>
2013-12-05 20:02:45 +0100

    First version with converting latitude/longitude into different formats

---
+++ Classes/ViewHelpers/CoordinateConverterViewHelper.php
@@ -0,0 +1,208 @@
+<?php
+namespace Byterror\BytCoordconverter\ViewHelpers;
+
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2013 Chris Müller <byt3error@web.de>
+*
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+* ************************************************************* */
+
+
+/**
+ * View helper for converting a geospatial coordinate into another
+ * format
+ *
+ * @author Chris Müller <byt3error@web.de>
+ * @package Byt_coordconverter
+ * @subpackage ViewHelpers\CoordConverter
+ */
+class CoordinateConverterViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+    /**
+     * The allowed coordinate formats
+     *
+     * @var array
+     */
+    protected $allowedFormats = array(
+        'degree',
+        'degreeMinutes',
+        'degreeMinutesSeconds',
+    );
+
+
+    /**
+     * Render method
+     *
+     * @param float $latitude
+     * @param float $longitude
+     * @param string $format
+     * @param string $cardinalPoints in format North South East West
+     * @param string $delimiter
+     * @param boolean $debug
+     * @return string
+     */
+    public function render($latitude, $longitude, $format = 'degree', $cardinalPoints = 'N|S|E|W', $delimiter = ', ', $showErrors = FALSE) {
+        $latitude = (float) $latitude;
+        $longitude = (float) $longitude;
+        $cardinalPointsArray = explode('|', $cardinalPoints);
+
+        if (($latitude > 90.0) || ($latitude < -90.0)) {
+            return $showErrors ? 'Wrong latitude: must be between 90.0 and -90.0 (given: ' . htmlspecialchars($latitude) . ')' : '';
+        }
+
+        if (($longitude > 180.0) || ($longitude < -180.0)) {
+            return $showErrors ? 'Wrong longitude: must be between 180.0 and -180.0 (given: ' . htmlspecialchars($longitude) . ')' : '';
+        }
+
+        if (count($cardinalPointsArray) !== 4) {
+            return $showErrors ? 'Wrong number of parameters for cardinal points: must be 4 (separated by |, given: ' . htmlspecialchars($cardinalPoints) . ')' : '';
+        }
+
+        if (!in_array($format, $this->allowedFormats)) {
+            return $showErrors ? 'Wrong format (given: ' . htmlspecialchars($format) . ', allowed: ' . implode(', ', $this->allowedFormats) . ')' : '';
+        }
+
+        $functionCall = 'get' . ucfirst($format) . 'Notation';
+
+        return $this->$functionCall($latitude, $longitude, $cardinalPointsArray, $delimiter);
+    }
+
+
+
+    /**
+     * Get the decimal notation
+     *
+     * @param float $latitude
+     * @param float $longitude
+     * @param array $cardinalPoints
+     * @param string $delimiter
+     * @return string
+     */
+    protected function getDegreeNotation($latitude, $longitude, $cardinalPoints, $delimiter) {
+        $content  = $this->getCardinalPointForLatitude($latitude, $cardinalPoints[0], $cardinalPoints[1]);
+        $content .= abs($latitude);
+
+        $content .= htmlspecialchars($delimiter);
+
+        $content .= $this->getCardinalPointForLongitude($longitude, $cardinalPoints[2], $cardinalPoints[3]);
+        $content .= abs($longitude);
+
+        return $content;
+    }
+
+
+
+    /**
+     * Get the degree with minutes notation
+     *
+     * @param float $latitude
+     * @param float $longitude
+     * @param array $cardinalPoints
+     * @param string $delimiter
+     * @return string
+     */
+    protected function getDegreeMinutesNotation($latitude, $longitude, $cardinalPoints, $delimiter) {
+        $newLatitudeDegrees = abs((int) $latitude);
+        $newLatitudeMinutes = abs(($latitude - (int) $latitude) * 60);
+
+        $newLongitudeDegrees = abs((int) $longitude);
+        $newLongitudeMinutes = abs(($longitude - (int) $longitude) * 60);
+
+        $content  = $this->getCardinalPointForLatitude($latitude, $cardinalPoints[0], $cardinalPoints[1]);
+        $content .= $newLatitudeDegrees . '°' . number_format($newLatitudeMinutes, 3) . '\'';
+
+        $content .= htmlspecialchars($delimiter);
+
+        $content .= $this->getCardinalPointForLongitude($latitude, $cardinalPoints[2], $cardinalPoints[3]);
+        $content .= $newLongitudeDegrees . '°' . number_format($newLongitudeMinutes, 3) . '\'';
+
+        return $content;
+    }
+
+
+
+    /**
+     * Get the degree with minutes and seconds notation
+     *
+     * @param float $latitude
+     * @param float $longitude
+     * @param array $cardinalPoints
+     * @param string $delimiter
+     * @return string
+     */
+    protected function getDegreeMinutesSecondsNotation($latitude, $longitude, $cardinalPoints, $delimiter) {
+        $newLatitudeDegrees = abs((int) $latitude);
+        $newLatitudeMinutes = abs(($latitude - (int) $latitude) * 60);
+        $newLatitudeSeconds = abs(($newLatitudeMinutes - (int) $newLatitudeMinutes) * 60);
+        $newLatitudeMinutes = (int) $newLatitudeMinutes;
+
+        $newLongitudeDegrees = abs((int) $longitude);
+        $newLongitudeMinutes = abs(($longitude - (int) $longitude) * 60);
+        $newLongitudeSeconds = abs(($newLongitudeMinutes - (int) $newLongitudeMinutes) * 60);
+        $newLongitudeMinutes = (int) $newLongitudeMinutes;
+
+        $content  = $this->getCardinalPointForLatitude($latitude, $cardinalPoints[0], $cardinalPoints[1]);
+        $content .= $newLatitudeDegrees . '° ' . $newLatitudeMinutes . '\' ' . number_format($newLatitudeSeconds, 2) . '"';
+
+        $content .= htmlspecialchars($delimiter);
+
+        $content .= $this->getCardinalPointForLongitude($latitude, $cardinalPoints[2], $cardinalPoints[3]);
+        $content .= $newLongitudeDegrees . '° ' . $newLongitudeMinutes . '\' ' . number_format($newLongitudeSeconds, 2) . '"';
+
+        return $content;
+    }
+
+
+
+    /**
+     * Get the cardinal point for a latitude
+     *
+     * @param float $latitude
+     * @param string $north
+     * @param string $south
+     * @return string
+     */
+    protected function getCardinalPointForLatitude($latitude, $north, $south) {
+        if ($latitude >= 0.0) {
+            return htmlspecialchars($north) . ' ';
+        }
+
+        return htmlspecialchars($south) . ' ';
+    }
+
+
+
+    /**
+     * Get the cardinal point for a longitude
+     *
+     * @param float $longitude
+     * @param string $east
+     * @param string $west
+     * @return string
+     */
+    protected function getCardinalPointForLongitude($longitude, $east, $west) {
+        if ($longitude >= 0.0) {
+            return htmlspecialchars($east) . ' ';
+        }
+
+        return htmlspecialchars($west) . ' ';
+    }
+}
---
+++ Tests/Unit/ViewHelpers/CoordinateConverterViewHelperTest.php
@@ -0,0 +1,335 @@
+<?php
+namespace Byterror\BytCoordconverter\Tests\Unit\ViewHelpers;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Chris Müller <byt3error@web.de>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+
+class CoordinateConverterViewHelperTest extends \TYPO3\CMS\Extbase\Tests\Unit\BaseTestCase {
+
+    /**
+     * @test
+     */
+    public function viewHelperFormatsDegreeCorrectly() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+        $actualResult = $viewHelper->render(
+            '49.487111',
+            '8.466278'
+        );
+        $this->assertEquals('N 49.487111, E 8.466278', $actualResult);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperFormatsDegreeMinutesCorrectly() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+        $actualResult = $viewHelper->render(
+            '49.487111',
+            '8.466278',
+            'degreeMinutes'
+        );
+        $this->assertEquals('N 49°29.227\', E 8°27.977\'', $actualResult);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperFormatsDegreeMinutesSecondsCorrectly() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+        $actualResult = $viewHelper->render(
+            '49.487111',
+            '8.466278',
+            'degreeMinutesSeconds'
+        );
+        $this->assertEquals('N 49° 29\' 13.60", E 8° 27\' 58.60"', $actualResult);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperUsesDelimiterCorrectly() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+        $actualResult = $viewHelper->render(
+            '49.487111',
+            '8.466278',
+            'degree',
+            'N|S|E|W',
+            ' / '
+        );
+        $this->assertEquals('N 49.487111 / E 8.466278', $actualResult);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperUsesCardinalPointsNorthAndEastCorrectly() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '49.487111',
+            '8.466278',
+            'degree',
+            'North|South|East|West'
+        );
+        $this->assertEquals('North 49.487111, East 8.466278', $actualResult);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperUsesCardinalPointsSouthAndWestCorrectly() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '-53.163494',
+            '-70.905071',
+            'degree',
+            'North|South|East|West'
+        );
+        $this->assertEquals('South 53.163494, West 70.905071', $actualResult);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperOutputsErrorWhenLatitudeExceedsPlus90DegreeAndErrorsShouldBeShown() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '90.01',
+            '0.0',
+            'degree',
+            'N|S|E|W',
+            ', ',
+            TRUE
+        );
+        $this->assertContains('wrong latitude', $actualResult, '', TRUE);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperOutputsErrorWhenLatitudeExceedsPlus90DegreeAndErrorsShouldBeSuppressed() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '90.01',
+            '0.0',
+            'degree',
+            'N|S|E|W',
+            ', ',
+            FALSE
+        );
+        $this->assertEmpty($actualResult);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperOutputsErrorWhenLatitudeExceedsMinus90DegreeAndErrorsShouldBeShown() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '-90.01',
+            '0.0',
+            'degree',
+            'N|S|E|W',
+            ', ',
+            TRUE
+        );
+        $this->assertContains('wrong latitude', $actualResult, '', TRUE);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperOutputsErrorWhenLatitudeExceedsMinus90DegreeAndErrorsShouldBeSuppressed() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '-90.01',
+            '0.0',
+            'degree',
+            'N|S|E|W',
+            ', ',
+            FALSE
+        );
+        $this->assertEmpty($actualResult);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperOutputsErrorWhenLongitudeExceedsPlus180DegreeAndErrorsShouldBeShown() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '0.0',
+            '180.01',
+            'degree',
+            'N|S|E|W',
+            ', ',
+            TRUE
+        );
+        $this->assertContains('wrong longitude', $actualResult, '', TRUE);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperOutputsErrorWhenLongitudeExceedsPlus180DegreeAndErrorsShouldBeSuppressed() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '0.0',
+            '180.01',
+            'degree',
+            'N|S|E|W',
+            ', ',
+            FALSE
+        );
+        $this->assertEmpty($actualResult);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperOutputsErrorWhenLongitudeExceedsMinus180DegreeAndErrorsShouldBeShown() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '0.0',
+            '-180.01',
+            'degree',
+            'N|S|E|W',
+            ', ',
+            TRUE
+        );
+        $this->assertContains('wrong longitude', $actualResult, '', TRUE);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperOutputsErrorWhenLongitudeExceedsMinus180DegreeAndErrorsShouldBeSuppressed() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '0.0',
+            '-180.01',
+            'degree',
+            'N|S|E|W',
+            ', ',
+            FALSE
+        );
+        $this->assertEmpty($actualResult);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperOutputsErrorWhenFormatIsWrongAndErrorsShouldBeShown() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '49.487111',
+            '8.466278',
+            'not existing',
+            'N|S|E|W',
+            ', ',
+            TRUE
+        );
+        $this->assertContains('wrong format', $actualResult, '', TRUE);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperOutputsErrorWhenFormatIsWrongAndErrorsShouldBeSuppressed() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '49.487111',
+            '8.466278',
+            'not existing',
+            'N|S|E|W',
+            ', ',
+            FALSE
+        );
+        $this->assertEmpty($actualResult);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperOutputsErrorWhenNumberOfParametersInCardinalPointsAreWrongAndErrorsShouldBeShown() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '49.487111',
+            '8.466278',
+            'degree',
+            'only|three|parameters',
+            ', ',
+            TRUE
+        );
+        $this->assertContains('wrong number', $actualResult, '', TRUE);
+    }
+
+
+    /**
+     * @test
+     */
+    public function viewHelperOutputsErrorWhenNumberOfParametersInCardinalPointsAreWrongAndErrorsShouldBeSuppressed() {
+        $viewHelper = new \Byterror\BytCoordconverter\ViewHelpers\CoordinateConverterViewHelper();
+
+        $actualResult = $viewHelper->render(
+            '49.487111',
+            '8.466278',
+            'degree',
+            'only|three|parameters',
+            ', ',
+            FALSE
+        );
+        $this->assertEmpty($actualResult);
+    }
+
+}
