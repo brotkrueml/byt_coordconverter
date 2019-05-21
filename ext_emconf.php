f6f1b551f6c2f13c5ee1d54d0654f40b1e05adcb
Chris Müller <chris@krue.ml>
2019-05-21 20:30:22 +0200

    [TASK] Raise TYPO3 version to use as minimum Fluid 2.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '8.7.25-9.5.99',
         ],
         'conflicts' => [
         ],
