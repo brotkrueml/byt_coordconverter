c5aa6ce18a94a8f3955bbad4582b31d0bc827d9e
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2019-10-02 14:42:52 +0200

    [TASK] Compatibility with TYPO3 v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,19 +1,17 @@
 <?php
 
-/** @noinspection PhpUndefinedVariableInspection */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Coordinate Converter',
     'description' => 'View helper for converting geospatial coordinates from one format into another',
     'category' => 'fe',
     'state' => 'stable',
-    'createDirs' => '',
     'clearCacheOnLoad' => true,
     'author' => 'Chris Müller',
     'author_email' => 'typo3@krue.ml',
     'version' => '2.1.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.25-10.0.99',
+            'typo3' => '8.7.25-10.4.99',
         ],
         'conflicts' => [
         ],
